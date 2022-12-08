743af9c9dbb6c5cc63224c56383787b2b3ab0898
Sidnei Aparecido Teixeira Batista <sidbalito@gmail.com>
2022-12-08 00:47:52 -0300

    Link na página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 export default function inicio(){
     return (<div>
         Monitoramento de consumo elétrico
+        <p><a href="/grafico">Gráfico dos registros mais recentes</a> </p>
         </div>)
 }
